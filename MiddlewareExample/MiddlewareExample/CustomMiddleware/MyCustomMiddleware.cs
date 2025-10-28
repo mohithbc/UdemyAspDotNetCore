3b13938d7b475985b559f157d252b5a06f8e54e5
mohithbc <mohithbc02@gmail.com>
2025-10-28 07:49:56 +0530

    Added MiddlewareExample

---
+++ MiddlewareExample/MiddlewareExample/CustomMiddleware/MyCustomMiddleware.cs
@@ -0,0 +1,13 @@
+﻿
+namespace MiddlewareExample.CustomMiddleware
+{
+    public class MyCustomMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            await context.Response.WriteAsync("Hello from MyCustomMiddleware! ");
+            await next(context);
+            await context.Response.WriteAsync("Goodbye from MyCustomMiddleware! ");
+        }
+    }
+}
--- MiddlewareExample/MiddlewareExample/Program.cs
+++ MiddlewareExample/MiddlewareExample/Program.cs
@@ -1,4 +1,7 @@
+using MiddlewareExample.CustomMiddleware;// import the namespace for custom middleware
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddTransient<MyCustomMiddleware>();// register custom middleware
 var app = builder.Build();
 
 // Code 1: The Consequence of Multiple app.Run Calls
@@ -14,6 +17,10 @@ var app = builder.Build();
 
 // Code 2: Chaining Middleware with app.Use and app.Run
 
+// if you use app.Run here, the next middleware will not be executed
+// because app.Run is a terminal middleware
+// if you want to chain middleware, use app.Use instead
+
 //middlware 1
 app.Use(async (HttpContext context,RequestDelegate next) => {
 await context.Response.WriteAsync("Hello ");
@@ -25,4 +32,12 @@ app.Use(async (context, next) => {
     await context.Response.WriteAsync("Hello again ");
     await next(context);
 });
+
+app.UseMiddleware<MyCustomMiddleware>();// use custom middleware
+
+//middleware 3
+app.Run(async (HttpContext context) => {
+    await context.Response.WriteAsync("Goodbye!");
+});
+
 app.Run();
