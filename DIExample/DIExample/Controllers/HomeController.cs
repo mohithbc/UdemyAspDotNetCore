5bab62aa1f7942a0f096d71fbc716b223ee98c86
mohithbc <mohithbc02@gmail.com>
2026-03-14 11:38:50 +0530

    DI example

---
+++ DIExample/DIExample/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services;
+
+namespace DIExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        //private readonly CitiesService _citiesService;
+        //// Constructor
+        //public HomeController(CitiesService citiesService)
+        //{
+        //    _citiesService = citiesService;
+        //}
+        [Route("/")]     
+        public IActionResult Index([FromServices]CitiesService _citiesService)
+        {     
+           List<string> cities = _citiesService.GetCities();
+            return View();
+        }
+    }
+}  
---
+++ DIExample/Services/CitiesService.cs
@@ -0,0 +1,22 @@
+﻿namespace Services
+{
+    public class CitiesService
+    {
+        private List<string> _cities;
+
+        public CitiesService() 
+        {
+            _cities = new List<string>()
+            {
+                "London",
+                "New York",
+                "Delhi"
+            };
+        }
+
+        public List<string> GetCities()
+        {
+            return _cities;
+        }
+    }
+}
