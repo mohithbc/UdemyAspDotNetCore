74864d804090b258c30a8d79d15a3186cd712b13
mohithbc <mohithbc02@gmail.com>
2026-03-14 17:52:07 +0530

    Environment Example

---
+++ EnvironmentsExample/EnvironmentsExample/Program.cs
@@ -0,0 +1,13 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+var app = builder.Build();
+
+if(app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+app.UseStaticFiles();
+app.UseRouting();
+app.MapControllers();
+
+app.Run();
